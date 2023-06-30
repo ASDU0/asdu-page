8954214cb130b22f09fba61032aba13d7c259dce
Giovdey <174440@unsaac.edu.pe>
2023-06-29 19:35:15 -0500

    Proyect

---
+++ public/scripts/scriptProyect.js
@@ -0,0 +1,15 @@
+var swiper=new Swiper(".tranding-slider",{
+    effect: "coverflow",
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:"auto",
+    coverflowEffect:{
+        rotate:15,
+        strech:0,
+        depth:300,
+        modificar:1,
+        slideShadows:true,
+    },
+    loop:true
+
+})

c0bb391029ebb363c2121731d66a9ce481eb914b
Giovdey <174440@unsaac.edu.pe>
2023-07-01 12:13:15 -0500

    Proyect slider avance

--- public/scripts/scriptProyect.js
+++ public/scripts/scriptProyect.js
@@ -1,15 +1,81 @@
-var swiper=new Swiper(".tranding-slider",{
-    effect: "coverflow",
-    grabCursor:true,
-    centeredSlides:true,
-    slidesPerView:"auto",
-    coverflowEffect:{
-        rotate:15,
-        strech:0,
-        depth:300,
-        modificar:1,
-        slideShadows:true,
-    },
-    loop:true
 
-})
+const dataProjects=[
+    {
+        image:"../img/image48.png",
+        name:'PROYECTO 1',
+        color:'B90909'
+    },
+    {
+        image:"../img/image49.png",
+        name:'PROYECTO 2',
+        color:'#052b3b'
+    },
+    {
+        image:"../img/image50.png",
+        name:'PROYECTO 3',
+        color:'#101a1b'
+    }
+]
+
+const cardProyects=document.querySelector('.cardsProyects');
+
+const postMethods=()=>{
+    dataProjects.map(({name,image,color})=>{
+        
+        const postElement=document.createElement('div');
+        postElement.classList.add('card-slider');
+        postElement.innerHTML=`
+            <div class="card-IMAGE">
+                <img src=${image}>
+            </div>
+            <div class="card-name">
+                <h1>${name}</h1>
+            </div>
+        `;
+        // console.log(postElement)
+        postElement.style.backgroundColor=color
+        cardProyects.appendChild(postElement);
+    })
+}
+postMethods()
+
+let items=document.querySelectorAll('.cardsProyects .card-slider');
+let next=document.getElementById('next');
+let prev=document.getElementById('prev');
+
+let active=0;
+function loadShow(){
+    let stt=0;
+    items[active].style.transform=`none`;
+    items[active].style.zIndex=1;
+    items[active].style.filter='none';
+    items[active].style.opacity=1;
+    items[active].style.transition='transform 0.4s'
+    for(var i=active +1; i<items.length;i++){
+        stt++;
+        items[i].style.transform=`translateX(${120*stt}px) scale(${1-0.2*stt}) perspective(16px) `;
+        items[i].style.zIndex=-stt;
+        items[i].style.filter='blur(3px)';
+        items[i].style.opacity=stt>2 ? 0 :0.6;
+    }
+    stt=0;
+    for(var i=active -1; i>=0;i--){
+        stt++;
+        items[i].style.transform=`translateX(${-120*stt}px) scale(${1-0.2*stt}) perspective(16px)`;
+        items[i].style.zIndex=-stt;
+        items[i].style.filter='blur(3px)';
+        items[i].style.opacity=stt>2 ? 0 :0.6;
+    }
+    
+
+}
+loadShow();
+
+next.onclick=function(){
+    active=active+1 <items.length? active+1:active;
+    loadShow();
+}
+prev.onclick=function(){
+    active=active-1 >=0?active-1:active;
+    loadShow();
+}

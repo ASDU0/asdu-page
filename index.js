ff7e6541e154a8f6d4f458ce870fd8031d1461ee
joel <52236148+joelinfo19@users.noreply.github.com>
2024-01-25 20:59:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path=require('path');
 const port=8000;
 
 app.use(express.static('public'));
-app.get('/asdu',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/views/index.html'))
 })
 
